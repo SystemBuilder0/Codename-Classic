1eff2b88b972be3fea1672dc34e2b0617beee9d7
SystemBuilder0 <89636202+SystemBuilder0@users.noreply.github.com>
2022-02-05 14:35:09 +0800

    Update First Code.c

--- First Code.c
+++ First Code.c
@@ -1,8 +1,8 @@
 //It is a fake program.
 #include <stdio.h>
 int main(){
-    puts("Hello, the User. You have no time to die. You must in my code world.");
-    puts("Ok, I'm kidding.)
+    printf("Hello, the User. You have no time to die. You must in my code world.");
+    printf("Ok, I'm kidding.");
     
     return 0;
 }
